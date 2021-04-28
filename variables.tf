c96506ed34941473092aecd8f6025d9e1b162ecc
root <root@DESKTOP-D06SP2I.localdomain>
2021-04-28 13:32:03 -0300

    Fix region-prefix-02

--- variables.tf
+++ variables.tf
@@ -1,10 +1,8 @@
 variable region  { 
     description = "Regiao usada para criar recursos"
-
 }
 
 variable prefix  { 
     description = "Regiao usada para criar recursos"
-    default = "jmulti"
 
 }
